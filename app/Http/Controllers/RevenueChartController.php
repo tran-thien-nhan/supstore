94c73a2ddcfb0c1372e56a72c1c48c234bae6ae3
tran-thien-nhan <pipclupnomad@gmail.com>
2023-08-16 12:50:36 +0700

    push code cho chart

--- app/Http/Controllers/RevenueChartController.php
+++ app/Http/Controllers/RevenueChartController.php
@@ -29,6 +29,8 @@ class RevenueChartController extends Controller
     public function index()
     {
         $this->Authenlogin();
+        $currentYear = now()->format('Y'); // Lấy năm hiện tại
+
         $dailyRevenue = DB::table('tbl_order')
             ->select(DB::raw('DATE(created_at) as date'), DB::raw('SUM(order_total) as total'))
             ->where('order_status', 3)
@@ -46,10 +48,75 @@ class RevenueChartController extends Controller
             ->orderBy('product_quantity', 'asc') // Sắp xếp tăng dần theo product_quantity
             ->first(); // Chọn sản phẩm có product_quantity thấp nhất (MIN)
 
+        $customerRegistrations = DB::table('tbl_customer')
+            ->select(DB::raw('MONTH(created_at) as month'), DB::raw('YEAR(created_at) as year'), DB::raw('COUNT(*) as registrations'))
+            ->whereYear('created_at', $currentYear)
+            ->groupBy('year', 'month')
+            ->get();
+
+        $customerRegistrationLabels = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+
+        $customerRegistrationData = [];
+        foreach ($customerRegistrationLabels as $monthLabel) {
+            $matchingMonth = $customerRegistrations->where('month', array_search($monthLabel, $customerRegistrationLabels) + 1)->first();
+
+            if ($matchingMonth) {
+                $customerRegistrationData[] = $matchingMonth->registrations;
+            } else {
+                $customerRegistrationData[] = 0;
+            }
+        }
+
+        $topCustomers = DB::table('tbl_customer')
+            ->orderBy('customer_point', 'desc')
+            ->limit(3) // Chọn top 3 người dùng
+            ->get();
+
+        $topProducts = DB::table('tbl_product')
+            ->orderBy('product_quantity', 'asc')
+            ->limit(5) // Chọn top 5 sản phẩm
+            ->get();
+
+
+
+        $monthlyRevenue = DB::table('tbl_order')
+            ->select(DB::raw('MONTH(created_at) as month'), DB::raw('SUM(order_total) as total'))
+            ->where('order_status', 3)
+            ->whereYear('created_at', $currentYear)
+            ->groupBy('month')
+            ->get();
+
+        // Bổ sung dữ liệu thiếu
+        $allMonths = range(1, 12); // Tạo một mảng chứa các tháng từ 1 đến 12
+
+        $monthlyData = [];
+
+        foreach ($allMonths as $month) {
+            $matchingMonth = $monthlyRevenue->where('month', $month)->first();
+
+            if ($matchingMonth) {
+                $monthlyData[] = $matchingMonth->total;
+            } else {
+                $monthlyData[] = 0; // Nếu không có dữ liệu, thêm giá trị 0
+            }
+        }
+
         return view('admin.dashboard')
             ->with('dailyRevenue', $dailyRevenue)
             ->with('mostPurchasedProduct', $mostPurchasedProduct)
             ->with('totalRevenueCurrentMonth', $totalRevenueCurrentMonth)
-            ->with('currentMonth', $currentMonth);
+            ->with('currentMonth', $currentMonth)
+            ->with('customerRegistrations', $customerRegistrations)
+            ->with('topCustomers', $topCustomers)
+            ->with('topProducts', $topProducts)
+            ->with('monthlyRevenue', $monthlyRevenue)
+            ->with('monthlyData', $monthlyData)
+            ->with('currentYear', $currentYear)
+            ->with('customerRegistrations', $customerRegistrations)
+            ->with('customerRegistrationLabels', $customerRegistrationLabels)
+            ->with('customerRegistrationData', $customerRegistrationData);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,12 +8,8 @@
                     <div class="col-lg-8">
                         <div class="info-container">
                             @if ($mostPurchasedProduct)
-                                <span class="badge bg-success text-white" style="font-size: 1rem; color:white"><span
-                                        class="text-warning"> &#9733;
-                                    </span>{{ $mostPurchasedProduct->product_name }}</span>
-                                <br><br>
                                 <p>Tổng Doanh Số Giao Hàng
-                                    Thành Công Trong Tháng {{$currentMonth}}: <span class="badge bg-success text-white"
+                                    Thành Công Trong Tháng {{ $currentMonth }}: <span class="badge bg-success text-white"
                                         style="font-size: 1rem; color: white">{{ number_format($totalRevenueCurrentMonth) }}</span>
                                 </p>
                             @else
@@ -23,7 +19,74 @@
                     </div>
                 </div>
                 <canvas id="revenueChart"></canvas>
+                <hr>
+                <h3 class="text-center">MONTHLY REGISTRATION RATE</h3>
+                <canvas id="registrationsChart"></canvas>
+                <hr>
+                <h3 class="text-center">MONTHLY REVENUE IN {{ $currentYear }}</h3>
+                <canvas id="monthlyRevenueChart"></canvas>
             </div>
+        </div>
+        <div class="top-customers col-lg-4">
+            <h3 class="text-center">TOP 3 CUSTOMERS</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th style="color:blue">Customer Name</th>
+                        <th style="color:blue">Points</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($topCustomers as $customer)
+                        <tr>
+                            <td style="font-weight:bold; color: black">{{ $customer->customer_name }}</td>
+                            <td><span class="badge bg-danger" style="font-size: 1rem">{{ $customer->customer_point }}</span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="top-products col-lg-4">
+            <h3 class="text-center">TOP 5 PRODUCTS SOLD</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th style="color:blue">Product Name</th>
+                        <th style="color:blue">Quantity Sold</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($topProducts as $product)
+                        <tr>
+                            <td style="font-weight:bold; color: black">{{ $product->product_name }}</td>
+                            <td><span class="badge bg-success"
+                                    style="font-size: 1rem;color:white">{{ 100 - $product->product_quantity }} sold</span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="customer-registrations col-lg-4">
+            <h3 class="text-center">CUSTOMER REGISTRATIONS</h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th style="color:blue">Month</th>
+                        <th style="color:blue">Registrations</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($customerRegistrations as $registration)
+                        <tr>
+                            <td style="font-weight:bold; color: black">{{ $registration->year }}-{{ str_pad($registration->month, 2, '0', STR_PAD_LEFT) }}</td>
+                            <td style="font-weight:bold; color: black">{{ $registration->registrations }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
@@ -54,6 +117,84 @@
                 }
             }
         });
+
+        var customerRegistrationLabels = @json($customerRegistrationLabels);
+        var customerRegistrationData = @json($customerRegistrationData);
+
+        var registrationsCtx = document.getElementById('registrationsChart').getContext('2d');
+        var registrationsChart = new Chart(registrationsCtx, {
+            type: 'line',
+            data: {
+                labels: customerRegistrationLabels,
+                datasets: [{
+                    label: 'Customer Registrations',
+                    data: customerRegistrationData,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 2,
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        var monthlyLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+            "October", "November", "December"
+        ]; // Tên các tháng
+        var monthlyData = @json($monthlyData);
+
+        var monthlyRevenueCtx = document.getElementById('monthlyRevenueChart').getContext('2d');
+        var monthlyRevenueChart = new Chart(monthlyRevenueCtx, {
+            type: 'bar',
+            data: {
+                labels: monthlyLabels,
+                datasets: [{
+                    label: 'Monthly Revenue',
+                    data: monthlyData,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        var monthlyLabels = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+            "October", "November", "December"
+        ];
+        var monthlyData = @json($monthlyRevenue->pluck('total'));
+
+        var monthlyRevenueCtx = document.getElementById('monthlyRevenueChart').getContext('2d');
+        var monthlyRevenueChart = new Chart(monthlyRevenueCtx, {
+            type: 'bar',
+            data: {
+                labels: monthlyLabels,
+                datasets: [{
+                    label: 'Monthly Revenue',
+                    data: monthlyData,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
     </script>
 @endsection
 
@@ -63,5 +204,66 @@
         padding: 10px;
         border: 1px solid #ccc;
         border-radius: 5px;
+        margin-bottom: 20px;
+    }
+
+    .top-customers {
+        margin-top: 20px;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: white;
+        /* Màu nền trắng */
+    }
+
+    .top-customers ul {
+        list-style: none;
+        padding-left: 0;
+    }
+
+    .top-customers li {
+        margin-bottom: 5px;
+    }
+
+    .top-customers h3 {
+        color: black;
+        /* Màu chữ đen */
+    }
+
+    .table th,
+    .table td {
+        color: black;
+        /* Màu chữ đen cho các ô trong bảng */
+    }
+
+    .top-products {
+        margin-top: 20px;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: white;
+    }
+
+    .top-products h3 {
+        color: black;
+    }
+
+    .table th,
+    .table td {
+        color: black;
+    }
+
+    .customer-registrations {
+        margin-top: 20px;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: white;
+        /* Màu nền trắng */
+    }
+
+    .customer-registrations h3 {
+        color: black;
+        /* Màu chữ đen */
     }
 </style>
